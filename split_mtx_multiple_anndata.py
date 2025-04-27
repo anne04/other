0eca550bbf85ca5806fe7c65e009eb139817ff3b
anne04 <anne.06.cse@gmail.com>
2025-04-26 22:15:20 -0400

    Update split_mtx_multiple_anndata.py

--- split_mtx_multiple_anndata.py
+++ split_mtx_multiple_anndata.py
@@ -1,6 +1,7 @@
 import scipy.io
-import pandas
+import pandas as pd
 import anndata
+import numpy as np
 
 
 temp = scipy.io.mmread('GSE206325_gene_vs_cell.mtx')
@@ -10,10 +11,17 @@ gene_names = pd.read_csv('gene_names.csv', header=None)
 gene_names = list(gene_names[0])
 cell_metadata = pd.read_csv('cell_metadata.csv') 
 
+cell_barcode_dict = dict()
+for barcode in cell_barcodes:
+    cell_barcode_dict[int(barcode.split('_')[0])] = 1
+
+
 patient_metadata = pd.read_csv('GSE206325_sample_annots_Liver_Treated_patients.csv') 
 tumor_R = []
 tumor_NR = []
 for i in range(0, len(patient_metadata)):
+    if patient_metadata['sample_ID'][i] not in cell_barcode_dict:
+        continue
     if patient_metadata['tissue'][i] == 'Tumor':# and patient_metadata['Immune_Infiltration'][i] == 'High':
         if patient_metadata['treatment_Resp'][i] == 'anti-PD1_NR':
             tumor_NR.append(patient_metadata['sample_ID'][i])
@@ -59,6 +67,7 @@ for sample_id in sample_id_list:
     anndata_list.append(adata)
     total_cells = total_cells + (end_column-start_column+1)
     #adata.write('sample'+ str(sample_id) +'.h5ad', compression="gzip")
+    print('total cells %d'%total_cells)
 
 print('total cells %d'%total_cells)
 adata = anndata.concat(anndata_list)
