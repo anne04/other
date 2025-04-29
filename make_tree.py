67c0c29ae77b85e22f6ecb066762adc8de0d6baa
anne04 <anne.06.cse@gmail.com>
2025-04-28 22:12:40 -0400

    Update make_tree.py

--- make_tree.py
+++ make_tree.py
@@ -7,14 +7,32 @@ from collections import defaultdict
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
 import numpy as np
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 cluster_type_info = pd.read_csv('GSE206325_full_HCC_cluster_annotation.csv')
 id_to_type = dict()
+
+# "type"
+'''
 for i in range (0, len(cluster_type_info)):
     id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['type'][i]
-    
 
-##
+triad_types = ['CD4', 'CD8', 'DC']
+for id in id_to_type.keys():
+    if id_to_type[id] not in triad_types:
+        id_to_type[id] = 'other'
+
+'''
+## "subgroup"
+for i in range (0, len(cluster_type_info)):
+    id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['subgroup'][i]
+    
+triad_types = ['1-Tfh-like', '2-Dysfunctional-progenitor', 'mregDC']
+for id in id_to_type.keys():
+    if id_to_type[id] not in triad_types:
+        id_to_type[id] = 'other'
+
 '''
 1-Tfh-like --- CD4 (out of these: 4-Effector-Memory, 5-Memory, 1-Tfh-like, 2-Th1 GZMK, 3-Th1/Th17)
 2-Dysfunctional-progenitor --- CD8 (out of these: 7-Cytotoxic, 3-Dysfunctional-Proliferating, 4-Dysfunctional-effector, 1-Dysfunctional-terminal, 2-Dysfunctional-progenitor, 5-Effector, 6-Memory)
@@ -30,16 +48,8 @@ sc.pp.highly_variable_genes(adata, n_top_genes=3000, subset=True)
 X = adata[:, adata.var['highly_variable']].X  # Use only highly variable genes
 y = list(adata.obs["cell_type"]) #adata.obs['leiden']                       # Your target labels
 
-
-
 for i in range(0, len(y)):
     y[i] = id_to_type[y[i]]
-
-triad_types = ['CD4', 'CD8', 'DC']
-for i in range(0, len(y)):
-    if y[i] not in triad_types: 
-        y[i] = 'other'
-
 
 # Step 2: Train-test split (optional, for evaluation)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
@@ -53,10 +63,13 @@ importances = clf.feature_importances_
 genes = adata[:, adata.var['highly_variable']].var_names
 
 # Step 5: Sort top marker genes
+gene_importance = defaultdict(list)
+
 marker_df = pd.DataFrame({'gene': genes, 'importance': importances})
 top_markers = marker_df.sort_values('importance', ascending=False).head(20)
-
 print(top_markers)
+
+# iterate over marker_df to get the importance of each feature
 
 ##################
 used_features = clf.tree_.feature
@@ -71,11 +84,14 @@ print(f"Number of unique genes used: {len(unique_features)}")
 print(f"Indices of features used: {unique_features}")
 
 gene_names = list(genes)
-selected_genes = [gene_names[i] for i in unique_features]
+selected_genes = []
+for i in unique_features:
+    selected_genes.append([gene_names[i], gene_importance])
 
+selected_genes = sorted(selected_genes, key = lambda x: x[1], reverse=True)
 print(f"Selected genes used in the tree:")
 for gene in selected_genes:
-    print(gene)
+    print("%s: %g"%(gene[0], gene[1]))
 ##################
 y_pred = clf.predict(X_test)
 sensitivity_per_class = recall_score(y_test, y_pred, average=None)
@@ -103,29 +119,9 @@ for label, spec in zip(clf.classes_, specificity_per_class):
 
 
 ###############
-from sklearn import tree
-import matplotlib.pyplot as plt
 
 plt.clf()
 plt.figure(figsize=(20,10))
 tree.plot_tree(clf, feature_names=genes, class_names=clf.classes_, filled=True, max_depth=10)
 plt.savefig('tree.svg')
    
-from sklearn.tree import DecisionTreeClassifier
-import numpy as np
-
-# Example: train your decision tree
-clf = DecisionTreeClassifier(max_depth=10, random_state=42)
-clf.fit(X_train, y_train)
-
-# Get feature indices used for splitting
-used_features = clf.tree_.feature
-
-# Remove -2 entries (they mean "leaf node", no split)
-used_features = used_features[used_features != -2]
-
-# Get unique features
-unique_features = np.unique(used_features)
-
-print(f"Number of unique genes used: {len(unique_features)}")
-print(f"Indices of features used: {unique_features}")
