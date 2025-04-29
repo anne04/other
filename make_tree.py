41920f9dad2677a49f97f4520d48062d2c985bf5
anne04 <anne.06.cse@gmail.com>
2025-04-29 09:51:40 -0400

    Update make_tree.py

--- make_tree.py
+++ make_tree.py
@@ -10,6 +10,8 @@ import numpy as np
 from sklearn import tree
 import matplotlib.pyplot as plt
 
+filter_gene = 1
+
 cluster_type_info = pd.read_csv('GSE206325_full_HCC_cluster_annotation.csv')
 id_to_type = dict()
 
@@ -33,6 +35,29 @@ for id in id_to_type.keys():
     if id_to_type[id] not in triad_types:
         id_to_type[id] = 'other'
 '''
+## "subgroup" and "type" combined
+for i in range (0, len(cluster_type_info)):
+    if cluster_type_info['type'][i] == 'DC':
+        if cluster_type_info['subgroup'][i] == 'mregDC':
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['subgroup'][i]
+        else:
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['type'][i]
+            
+    elif cluster_type_info['type'][i] == 'CD8':
+        if cluster_type_info['subgroup'][i] == '2-Dysfunctional-progenitor':
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['subgroup'][i]
+        else:
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['type'][i]
+            
+    elif cluster_type_info['type'][i] == 'CD4':
+        if cluster_type_info['subgroup'][i] == '1-Tfh-like':
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['subgroup'][i]
+        else:
+            id_to_type[cluster_type_info['cluster_ID'][i]] = cluster_type_info['type'][i]
+    else:
+        id_to_type[cluster_type_info['cluster_ID'][i]] = 'other'
+
+
 '''
 1-Tfh-like --- CD4 (out of these: 4-Effector-Memory, 5-Memory, 1-Tfh-like, 2-Th1 GZMK, 3-Th1/Th17)
 2-Dysfunctional-progenitor --- CD8 (out of these: 7-Cytotoxic, 3-Dysfunctional-Proliferating, 4-Dysfunctional-effector, 1-Dysfunctional-terminal, 2-Dysfunctional-progenitor, 5-Effector, 6-Memory)
@@ -57,13 +82,14 @@ if filter_gene == 1:
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
     X = adata.X  
+    gene_names = adata.var_names #adata[:, adata.var['highly_variable']].var_names
 else:
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(adata, n_top_genes=3000, subset=True)
     # Step 1: Use the raw gene expression matrix
     X = adata[:, adata.var['highly_variable']].X  # Use only highly variable genes
-
+    gene_names = adata[:, adata.var['highly_variable']].var_names
 
 y = list(adata.obs["cell_type"]) #adata.obs['leiden']                       # Your target labels
 
@@ -107,13 +133,13 @@ for label, spec in zip(clf.classes_, specificity_per_class):
 
 
 
+
+
 # Step 4: Get feature (gene) importance
 importances = clf.feature_importances_
-genes = adata.var_names #adata[:, adata.var['highly_variable']].var_names
-
 # Step 5: Sort top marker genes
 gene_importance = dict()
-marker_df = pd.DataFrame({'gene': genes, 'importance': importances})
+marker_df = pd.DataFrame({'gene': gene_names, 'importance': importances})
 top_markers = marker_df.sort_values('importance', ascending=False).head(20)
 print(top_markers)
 
@@ -129,7 +155,7 @@ unique_features = np.unique(used_features)
 print(f"Number of unique genes used: {len(unique_features)}")
 #print(f"Indices of features used: {unique_features}")
 
-gene_names = list(genes)
+gene_names = list(gene_names)
 selected_genes = []
 for i in unique_features:
     selected_genes.append([gene_names[i], gene_importance[gene_names[i]]])
@@ -143,7 +169,7 @@ for gene in selected_genes:
 
 plt.clf()
 plt.figure(figsize=(20,20))
-tree.plot_tree(clf, feature_names=genes, class_names=clf.classes_, filled=True, max_depth=10)
-plt.savefig('tree_subgroup_filtered.svg')
-plt.savefig('tree_subgroup_nofilter.svg')
-   
+tree.plot_tree(clf, feature_names=gene_names, class_names=clf.classes_, filled=True, max_depth=10)
+#plt.savefig('tree_subgroup_filtered.svg')
+#plt.savefig('tree_subgroup_nofilter.svg')
+plt.savefig('tree_type_n_subgroup_filtered.svg')   
